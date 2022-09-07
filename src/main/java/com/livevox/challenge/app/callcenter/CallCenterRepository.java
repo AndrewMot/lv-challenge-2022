af0f3557ae90bb014c4cda60c3ac9ab773dd21a9
Andres Motavita <amotavita@twilio.com>
2022-09-07 12:56:38 -0500

    Create Call Center

--- src/main/java/com/livevox/challenge/app/callcenter/CallCenterRepository.java
+++ src/main/java/com/livevox/challenge/app/callcenter/CallCenterRepository.java
@@ -1,5 +1,15 @@
 package com.livevox.challenge.app.callcenter;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
-public interface CallCenterRepository extends JpaRepository<CallCenter, Long> {}
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface CallCenterRepository extends JpaRepository<CallCenter, Long> {
+
+    @Query("SELECT callCenter FROM CallCenter callCenter WHERE LOWER(callCenter.name) = LOWER(:name) AND LOWER" +
+           "(callCenter.countryName) = LOWER(:countryName) ")
+    Optional<CallCenter> findByNameAndCountryName(@Param("name") final String name,
+                                                  @Param("countryName") final String countryName);
+}
---
+++ src/main/java/com/livevox/challenge/app/callcenter/CallCenterService.java
@@ -0,0 +1,36 @@
+package com.livevox.challenge.app.callcenter;
+
+import java.util.Optional;
+
+import com.livevox.challenge.app.response.Constants;
+import com.livevox.challenge.app.response.exceptions.BadRequestException;
+import com.livevox.challenge.app.response.exceptions.ConflictException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+@Service
+public class CallCenterService {
+
+    private final CallCenterRepository callCenterRepository;
+
+    @Autowired
+    public CallCenterService(final CallCenterRepository callCenterRepository) {
+        this.callCenterRepository = callCenterRepository;
+    }
+
+    public CallCenter create(final CallCenter callCenter) {
+        if (!StringUtils.hasLength(callCenter.getName())) {
+            throw new BadRequestException(Constants.CALL_CENTER_NAME_REQUIRED_MESSAGE);
+        }
+        if (!StringUtils.hasLength(callCenter.getCountryName())) {
+            throw new BadRequestException(Constants.CALL_CENTER_COUNTRY_NAME_REQUIRED_MESSAGE);
+        }
+        final Optional<CallCenter> duplicatedNameInCountry =
+            callCenterRepository.findByNameAndCountryName(callCenter.getName(), callCenter.getCountryName());
+        if (duplicatedNameInCountry.isPresent()) {
+            throw new ConflictException(Constants.CALL_CENTER_UNIQUE_NAME_MESSAGE);
+        }
+        return callCenterRepository.save(callCenter);
+    }
+}
