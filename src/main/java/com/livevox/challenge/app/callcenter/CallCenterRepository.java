4614c5a6fd9792a737729ff74b2bb7deaa51f968
Andres Motavita <amotavita@twilio.com>
2022-09-07 12:59:29 -0500

    Revert "Create Call Center"
    This reverts commit af0f3557ae90bb014c4cda60c3ac9ab773dd21a9.

--- src/main/java/com/livevox/challenge/app/callcenter/CallCenterRepository.java
+++ src/main/java/com/livevox/challenge/app/callcenter/CallCenterRepository.java
@@ -1,15 +1,5 @@
 package com.livevox.challenge.app.callcenter;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-public interface CallCenterRepository extends JpaRepository<CallCenter, Long> {
-
-    @Query("SELECT callCenter FROM CallCenter callCenter WHERE LOWER(callCenter.name) = LOWER(:name) AND LOWER" +
-           "(callCenter.countryName) = LOWER(:countryName) ")
-    Optional<CallCenter> findByNameAndCountryName(@Param("name") final String name,
-                                                  @Param("countryName") final String countryName);
-}
+public interface CallCenterRepository extends JpaRepository<CallCenter, Long> {}
--- src/main/java/com/livevox/challenge/app/response/Constants.java
+++ src/main/java/com/livevox/challenge/app/response/Constants.java
@@ -5,11 +5,7 @@ public final class Constants {
     public static final String CALL_CENTER_ID_REQUIRED_MESSAGE = "Call center id is required";
     public static final String AGENT_NOT_FOUND_MESSAGE = "Agent not found";
     public static final String CALL_CENTER_NOT_FOUND_MESSAGE = "Call center doesn't exist";
-    public static final String UNIQUE_EXTENSION_MESSAGE =
-        "An agent with same extension is already assigned to call center";
-    public static final String CALL_CENTER_NAME_REQUIRED_MESSAGE = "Name is required";
-    public static final String CALL_CENTER_COUNTRY_NAME_REQUIRED_MESSAGE = "Country name is required";
-    public static final String CALL_CENTER_UNIQUE_NAME_MESSAGE = "Name must be unique per country";
+    public static final String UNIQUE_EXTENSION_MESSAGE = "An agent with same extension is already assigned to call center";
 
     private Constants() {
         throw new UnsupportedOperationException("Utility Class");
